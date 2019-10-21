dd532372365de57cc6da01637dc973632b689dd7
R.G <reflechant@users.noreply.github.com>
2019-10-21 12:44:04 +0300

    Joined find methods into one.

--- bst.py
+++ bst.py
@@ -1,6 +1,6 @@
 from typing import Union, Tuple, Generic, TypeVar
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 class Node(Generic[T]):
@@ -8,54 +8,59 @@ class Node(Generic[T]):
     Binary search tree implementation. Duplicates not allowed. Keys are values.
     """
 
-    def __eq__(self, n: 'Node') -> bool:
+    def __eq__(self, n: "Node") -> bool:
         return self.val == n.val and self.left is n.left and self.right is n.right
 
-    def __init__(self, val: T, left: 'Node' = None, right: 'Node' = None):
+    def __init__(self, val: T, left: "Node" = None, right: "Node" = None):
         self.val = val
         self.left: Node = left
         self.right: Node = right
 
-    def find_parent(self, n: 'Node') -> 'Node':
-        """
-        Returns parent node of n, considering self is tree root
-        """
+    def __find(
+        self, el: Union["Node", T], no_fail=False, find_parent=False
+    ) -> Union["Node", None]:
         cur_node = self
-        while True:
-            if cur_node.left is n or cur_node.right is n:
-                return cur_node
-            elif cur_node.left and n.val < cur_node.val:
-                cur_node = cur_node.left
-            elif cur_node.right and n.val > cur_node.val:
-                cur_node = cur_node.right
+        if isinstance(el, Node):
+            if find_parent:
+                eq = lambda n: n.left is el or n.right is el
             else:
-                raise LookupError
-
-    def __find(self, x: T, return_nearest=False) -> Union['Node', None]:
-        cur_node = self
+                eq = lambda n: el is n
+            lt = lambda n: el.val < n.val
+            gt = lambda n: el.val > n.val
+        else:
+            if find_parent:
+                eq = lambda n: el == n.right.val or el == n.left.val
+            else:
+                eq = lambda n: el == n.val
+            lt = lambda n: el < n.val
+            gt = lambda n: el > n.val
         while True:
-            if x < cur_node.val and cur_node.left:
+            if eq(cur_node):
+                return cur_node
+            elif cur_node.left and lt(cur_node):
                 cur_node = cur_node.left
-            elif x > cur_node.val and cur_node.right:
+            elif cur_node.right and gt(cur_node):
                 cur_node = cur_node.right
-            elif x == cur_node.val or return_nearest:
+            elif no_fail:
                 return cur_node
             else:
                 return None
 
-    def find(self, x) -> Union['Node', None]:
-        return self.__find(x)
+    def find(self, val) -> Union["Node", None]:
+        return self.__find(val)
 
-    def find_nearest(self, x) -> 'Node':
-        return self.__find(x, return_nearest=True)
+    def find_nearest(self, val) -> "Node":
+        return self.__find(val, no_fail=True)
 
-    def remove_value(self, x: T):
-        n = self.find(x)
-        if n:
-            self.remove_node(n)
+    def find_parent(self, el: Union["Node", T]) -> "Node":
+        return self.__find(el, find_parent=True)
 
-    def remove_node(self, n: 'Node'):
-        p = self.find_parent(n)
+    def remove(self, el: Union["Node", T]):
+        p = self.__find(el, find_parent=True)
+        if isinstance(el, Node):
+            n = el
+        else:
+            n = p.left if p.left and p.left.val == el else p.right
         if n.left and n.right:
             pass
         elif n.left:
@@ -65,22 +70,26 @@ class Node(Generic[T]):
         else:
             pass
 
-    def add(self, x: T) -> 'Node':
-        n = self.find_nearest(x)
-        if x < n.val:
-            n.left = Node(x)
-        elif x > n.val:
-            n.right = Node(x)
+    def add(self, val: T) -> "Node":
+        n = self.find_nearest(val)
+        if val < n.val:
+            n.left = Node(val)
+        elif val > n.val:
+            n.right = Node(val)
         return n
 
-    def contains(self, x: T) -> bool:
-        return True if self.__find(x) else False
+    def contains(self, val: T) -> bool:
+        return True if self.__find(val) else False
 
     def is_valid_bst(self) -> bool:
         pass
 
     def tuple(self) -> Tuple[T, tuple, tuple]:
-        return self.val, self.left.tuple() if self.left else None, self.right.tuple() if self.right else None
+        return (
+            self.val,
+            self.left.tuple() if self.left else None,
+            self.right.tuple() if self.right else None,
+        )
 
     def __iter__(self):
         def f():
