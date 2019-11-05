1fa688d9de36bb48c0e2cab29d385774c5305f18
R.G <reflechant@users.noreply.github.com>
2019-11-06 01:26:11 +0300

    major refactoring, deleted redundant methods

--- bst.py
+++ bst.py
@@ -11,136 +11,75 @@ class Node(Generic[T]):
     """
 
     def __eq__(self, n: "Node") -> bool:
-        return self.val == n.val and self.left is n.left and self.right is n.right
+        return all(map(lambda pair: pair[0] == pair[1], zip(self, n)))
 
-    def __init__(self, val: T, left: "Node" = None, right: "Node" = None):
-        self.val = val
+    def __init__(self, key: T, left: "Node" = None, right: "Node" = None):
+        self.key = key
         self.left: Node = left
         self.right: Node = right
 
-    def __find_parent_of_node(self, n: "Node") -> "Node":
-        """
-        Returns parent node of node n, considering self is tree root
-        """
+    def find(self, key: T) -> Union["Node", None]:
         node = self
-        while True:
-            if node.left is n or node.right is n:
-                return node
-            if node.left and n.val < node.val:
+        while node:
+            if key < node.key:
                 node = node.left
-            if node.right and n.val > node.val:
+            elif key > node.key:
                 node = node.right
-            if not node.left and not node.right:
-                return None
+            elif key == node.key:
+                return node
+        return None
 
-    def __find_parent_of_value(self, x: T) -> "Node":
-        """
-        Returns parent node of value x, considering self is tree root
-        """
+    def __contains__(self, key: T) -> bool:
+        if self.find(key):
+            return True
+        return False
+
+    def __remove_node(self, node: "Node"):
+        pass
+
+    def remove(self, key: T):
         node = self
-        while True:
-            if node.left:
-                if x == node.left.val:
-                    return node
-                elif x < node.left.val:
+        while node:
+            if key == node.key:
+                # self.__remove_node(node)
+                break
+            elif key < node.key:
+                node = node.left
+            else:
+                node = node.right
+
+    def insert(self, key: T) -> "Node":
+        node = self
+        while node:
+            if key == node.key:
+                return None
+            elif key < node.key:
+                if node.left:
                     node = node.left
-            if node.right:
-                if x == node.right.val:
-                    return node
-                elif x > node.right.val:
+                else:
+                    node.left = Node(key)
+                    return node.left
+            elif key > node.key:
+                if node.right:
                     node = node.right
-            if not node.left and not node.right:
-                return None
+                else:
+                    node.right = Node(key)
+                    return node.right
 
-    def __find(self, x: T, no_fail=False) -> Union["Node", None]:
-        node = self
-        while True:
-            if x < node.val and node.left:
-                node = node.left
-            elif x > node.val and node.right:
-                node = node.right
-            elif x == node.val or no_fail:
-                return node
-            else:
-                return None
-
-    def find(self, x) -> Union["Node", None]:
-        return self.__find(x)
-
-    def __find_nearest(self, x) -> "Node":
-        return self.__find(x, no_fail=True)
-
-    def find_parent(self, el: Union["Node", T]):
-        if isinstance(el, Node):
-            return self.__find_parent_of_node(el)
-        else:
-            return self.__find_parent_of_value(el)
-
-    def __remove_value(self, x: T):
-        p = self.__find_parent_of_value(x)
-        if not p:
-            return
-        if x < p.val:
-            self.__remove_node(p.left)
-        else:
-            self.__remove_node(p.right)
-
-    def __remove_node(self, n: "Node"):
-        p = self.__find_parent_of_node(n)
-        if not p:
-            return
-        if n.left and n.right:
-            pass
-        elif n.left:
-            pass
-        elif n.right:
-            pass
-        else:
-            if p.left is n:
-                p.left = None
-            else:
-                p.right = None
-
-    def remove(self, el: Union["Node", T]):
-        if isinstance(el, Node):
-            self.__remove_node(el)
-        else:
-            self.__remove_value(el)
-
-    def add(self, x: T) -> "Node":
-        n = self.__find_nearest(x)
-        if x < n.val:
-            n.left = Node(x)
-            return n.left
-        elif x > n.val:
-            n.right = Node(x)
-            return n.right
-
-    def contains(self, x: T) -> bool:
-        return True if self.__find(x) else False
-
-    def is_valid_bst(self) -> bool:
+    def validate(self) -> bool:
         a, b = tee(self.__iter__())
         next(b, None)
         return all(lt(x, y) for x, y in zip(a, b))
-
-    def tuple(self) -> Tuple[T, tuple, tuple]:
-        return (
-            self.val,
-            self.left.tuple() if self.left else None,
-            self.right.tuple() if self.right else None,
-        )
 
     def __iter__(self):
         def f():
             if self.left:
                 yield from self.left
-            yield self.val
+            yield self.key
             if self.right:
                 yield from self.right
 
         return f()
 
     def __repr__(self):
-        return "-".join(str(x) for x in iter(self))
-
+        return " ".join(str(x) for x in iter(self))
--- bst_test.py
+++ bst_test.py
@@ -4,99 +4,67 @@ from bst import Node
 def test_init():
     root = Node(0)
     assert isinstance(root, Node)
-    assert root.val == 0
+    assert root.key == 0
     assert root.left is None
     assert root.right is None
 
 
-def test_node_to_tuple():
-    assert Node(0).tuple() == (0, None, None)
-    assert Node(1, Node(0)).tuple() == (1, (0, None, None), None)
-    assert Node(1, None, Node(2)).tuple() == (1, None, (2, None, None))
-    assert Node(1, Node(0), Node(2)).tuple() == (1, (0, None, None), (2, None, None))
-
-
 def test_node_repr():
     assert str(Node(0)) == "0"
-    assert str(Node(1, Node(0), Node(2))) == "0-1-2"
+    assert str(Node(1, Node(0), Node(2))) == "0 1 2"
 
 
-def test_add_duplicate():
+def test_insert_duplicate():
     root = Node(1)
-    root.add(1)
+    root.insert(1)
     assert root == Node(1)
 
 
-def test_add_left():
+def test_insert_left():
     root = Node(1)
-    root.add(0)
-    assert root.tuple() == (1, (0, None, None), None)
+    root.insert(0)
+    assert str(root) == "0 1"
 
 
-def test_add_right():
+def test_insert_right():
     root = Node(1)
-    root.add(2)
-    assert root.tuple() == Node(1, None, Node(2)).tuple()
+    root.insert(2)
+    assert str(root) == "1 2"
 
 
-def test_contains():
+def test_in():
     root = Node(1)
-    root.add(2)
-    root.add(3)
-    assert root.contains(0) == False
-    assert root.contains(1) == True
-    assert root.contains(3) == True
+    root.insert(2)
+    root.insert(3)
+    assert (0 in root) == False
+    assert (1 in root) == True
+    assert (3 in root) == True
 
 
 def test_find():
     root = Node(1)
-    root.add(2)
-    root.add(-5)
+    root.insert(2)
+    root.insert(-5)
     assert root.find(1) is root
     assert root.find(100) is None
     assert root.find(2) is root.right
     assert root.find(-5) is root.left
 
 
-def test_find_nearest():
-    root = Node(5)
-    assert root.__find_nearest(1) is root
-    assert root.__find_nearest(100) is root
-
-
-def test_find_parent():
-    root = Node(5)
-    assert root.find_parent(5) is None
-    root.add(2)
-    root.add(7)
-    assert root.find_parent(root.left) is root
-    assert root.find_parent(root.right) is root
-    assert root.find_parent(2) is root
-    assert root.find_parent(7) is root
-
-
-def test_remove_value():
+def test_remove():
     root = Node(1, Node(0), Node(2))
     root.remove(0)
-    assert root.tuple() == (1, None, (2, None, None))
+    assert str(root) == "1 2"
 
 
-def test_remove_node():
-    root = Node(1)
-    root.add(2)
-    n = root.add(3)
-    root.remove(n)
-    assert root.tuple() == (1, None, (2, None, None))
-
-
-def test_valid_bst():
+def test_validate():
     root = Node(1, Node(2))
-    assert not root.is_valid_bst()
+    assert not root.validate()
     root = Node(1, Node(0))
-    assert root.is_valid_bst()
+    assert root.validate()
     root = Node(1, None, Node(0))
-    assert not root.is_valid_bst()
+    assert not root.validate()
     root = Node(1, None, Node(2))
-    assert root.is_valid_bst()
+    assert root.validate()
     root = Node(1, Node(0), Node(2))
-    assert root.is_valid_bst()
+    assert root.validate()
